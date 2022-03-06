86906aab1f6f3fb69c6d79b7944971ac768dc246
AliRehanCodes <alirehancodes@gmail.com>
2022-03-07 01:00:27 +0500

    finger tracking counter

--- handDetector.py
+++ handDetector.py
@@ -1,5 +1,5 @@
 import mediapipe as mp
-import cv2
+import cv2,time
 
 class handDetector():
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5,modelComplexity=1,trackCon=0.5):
@@ -52,3 +52,28 @@ class handDetector():
                     cv2.circle(img,(cx,cy), 7 , (255,0,255), cv2.FILLED)
 
         return lmlist
+    
+    def main(self):
+        #Frame rates
+        pTime = 0
+        cTime = 0
+        cap = cv2.VideoCapture(0)
+        while True:
+            success,img = cap.read()
+            img = self.findHands(img)
+            lmList = self.findPosition(img)
+            if len(lmList) != 0:
+                print(lmList[4])
+
+            cTime=time.time()
+            fps=1/(cTime-pTime)
+            pTime=cTime
+            cv2.putText(img, f'FPS: {int(fps)}',(400,70),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,0),3)
+            cv2.imshow("image",img)
+
+            cv2.imshow("Video",img)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
